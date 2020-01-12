de82a188f86f8da62d4a952b698107e2f2c1081c
eulaliecoevoet <eulalie.coevoet@gmail.com>
2020-01-11 22:03:56 -0500

    [scenes3D] minor changes for the cart example to look better

--- Sleeping and Merging Bodies/MergingBodiesProj/scenes3D/python/chariot.py
+++ Sleeping and Merging Bodies/MergingBodiesProj/scenes3D/python/chariot.py
@@ -11,8 +11,8 @@ root = ET.Element('root')
 # Plane
 plane = ET.SubElement(root, 'body')
 plane.set('type','plane')
-plane.set('p','0 -1 0')
-plane.set('n','-0.1 1. 0.0')
+plane.set('p','0 -0.6 0')
+plane.set('n','-0.05 1. 0.0')
 plane.set('name','plane')
 fric = ET.SubElement(plane, 'friction')
 fric.text = "0.01"
@@ -25,15 +25,16 @@ z0 = -1.9
 y=y0; x=x0; z=z0;
 dimx=0.5; dimy=0.5; dimz=0.5;
 for i in range(216):
-    y += dimy + 0.05
+    y += dimy + 0.01
     if (i%6==0):
-        x+=dimx + 0.05; y=y0;
+        x+=dimx + 0.01; y=y0;
     if (i%36==0):
-        x=x0; y=y0; z+=dimz + 0.05;
+        x=x0; y=y0; z+=dimz + 0.01;
+    orientation = "0 -1 0 0.03" if (i%2) else "0 -1 0 0";
     Box(root, name='box'+str(i), position=str(x)+" "+str(y)+" "+str(z), dim=str(dimx)+" "+str(dimy)+" "+str(dimz))
 
-Sphere(root, name='ball', position="2.5 3 0", radius="1")
-Box(root, name='box'+str(i), position="-30 0 0", dim="0.2 20 20", pinned="true")
+Sphere(root, name='ball', position="1.8 2.7 0", radius="1.")
+Box(root, name='box'+str(i), position="-30 -2 0", orientation="0 0 1 0.08", dim="10 5 10", pinned="true")
 
 # chariot
 composite = CompositeBody(root, obj="data/chariot.obj", scale="0.1", name="chariot", position="0. 1.7 0.", velocity="0. 0. 0.")
